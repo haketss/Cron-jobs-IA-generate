835edc3372ff15a0b315e51f532dc33425c5bbe8
haketss <ruanrudrigues9@gmail.com>
2025-09-12 11:54:53 -0400

    porta ajustada

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 import 'dotenv/config'
+const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(port),
   DATABASE_URL: z.string().url().default(process.env.DATABASE_URL ?? ""),
   GEMINI_API_KEY: z.string().default(process.env.GEMINI_API_KEY ?? ""),
 });
